88a9ed2ab532050ab91cce24a043b5ec51c7bd08
ansobolev <andrey.n.sobolev@gmail.com>
2014-11-26 17:04:51 +0500

    Initial directory structure

---
+++ scripts/example.py
@@ -0,0 +1,33 @@
+__author__ = 'andrey'
+
+import vtk
+
+# create a rendering window and renderer
+ren = vtk.vtkRenderer()
+renWin = vtk.vtkRenderWindow()
+renWin.AddRenderer(ren)
+
+# create a renderwindowinteractor
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renWin)
+
+# create source
+source = vtk.vtkSphereSource()
+source.SetCenter(0,0,0)
+source.SetRadius(5.0)
+
+# mapper
+mapper = vtk.vtkPolyDataMapper()
+mapper.SetInputConnection(source.GetOutputPort())
+
+# actor
+actor = vtk.vtkActor()
+actor.SetMapper(mapper)
+
+# assign actor to the renderer
+ren.AddActor(actor)
+
+# enable user interface interactor
+iren.Initialize()
+renWin.Render()
+iren.Start()
