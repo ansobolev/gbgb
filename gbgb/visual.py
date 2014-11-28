ee7b9e605f85526bc2a3ae8613aa06b59bb98d74
ansobolev <andrey.n.sobolev@gmail.com>
2014-11-28 16:25:14 +0500

    added vector visualizer

--- gbgb/visual.py
+++ gbgb/visual.py
@@ -11,6 +11,7 @@ __author__ = 'andrey'
 """ Visualization with Enthought Mayavi package
 """
 
+import numpy as np
 try:
     from enthought.mayavi import mlab
 except ImportError:
@@ -21,8 +22,16 @@ class Visual(object):
     """
     Generic class for visualizers
     """
+    _sources = {}
+
     def __init__(self):
         pass
+
+    def get_names(self):
+        return self._sources.keys()
+
+    def get_sources(self):
+        return self._sources.iteritems()
 
     @staticmethod
     def show():
@@ -33,10 +42,20 @@ class AtomsVisual(Visual):
     """ Visualization of atoms by their Cartesian coordinates
     """
 
-    def __init__(self, crd):
+    def __init__(self, name, crd):
         super(AtomsVisual, self).__init__()
-        mlab.points3d(crd[:, 0], crd[:, 1], crd[:, 2])
+        self._sources["at_" + name] = mlab.points3d(crd[:, 0], crd[:, 1], crd[:, 2])
 
-    @staticmethod
-    def add_atoms(crd, color):
-        mlab.points3d(crd[:, 0], crd[:, 1], crd[:, 2], color=color)
+    def add_atoms(self, name, crd, color):
+        self._sources["at_" + name] = mlab.points3d(crd[:, 0], crd[:, 1], crd[:, 2], color=color)
+
+
+class VectorVisual(Visual):
+    """ Vector visualization
+    """
+
+    def __init__(self, name, crd):
+        super(VectorVisual, self).__init__()
+        vectors = np.hstack((np.zeros_like(crd), crd))
+        self._sources["v_" + name] = mlab.quiver3d(*vectors.T)
+
